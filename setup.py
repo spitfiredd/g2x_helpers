16a8ea8971527e9b1beda06a1b097a1cbeb2351b
Daniel Donovan <spitfiredd@gmail.com>
2017-08-23 12:40:54 -0400

    added xmltodict to install requires

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='g2x_helpers',
-      version='0.1',
+      version='0.2',
       description='Helper Functions used across different packages',
       author='Daniel Donovan',
       author_email='spitfiredd@gmail.com',
       url='https://github.com/spitfiredd/g2x_helpers',
       license='MIT',
       packages=['g2x_helpers', 'g2x_helpers.pysam', 'g2x_helpers.pyfpds'],
-      install_requires=['requests'],
+      install_requires=['requests', 'xmltodict'],
       zip_safe=False)
